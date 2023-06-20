d3e330798cd722681c9fb33f7ba8b64b03461561
Jeffrey Lim <jeff@lim.au>
2023-06-21 05:02:36 +0800

    Increase max block size, fixing not restarting after writing large flash amounts

--- src/usb/uf2/uf2.h
+++ src/usb/uf2/uf2.h
@@ -32,51 +32,51 @@ SOFTWARE.
 
 #include "uf2cfg.h"
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 //--------------------------------------------------------------------+
 //
 //--------------------------------------------------------------------+
 
 // All entries are little endian.
-#define UF2_MAGIC_START0  0x0A324655UL // "UF2\n"
-#define UF2_MAGIC_START1  0x9E5D5157UL // Randomly selected
-#define UF2_MAGIC_END     0x0AB16F30UL // Ditto
+#define UF2_MAGIC_START0 0x0A324655UL // "UF2\n"
+#define UF2_MAGIC_START1 0x9E5D5157UL // Randomly selected
+#define UF2_MAGIC_END 0x0AB16F30UL    // Ditto
 
 // If set, the block is "comment" and should not be flashed to the device
-#define UF2_FLAG_NOFLASH  0x00000001
+#define UF2_FLAG_NOFLASH 0x00000001
 #define UF2_FLAG_FAMILYID 0x00002000
 
-#define MAX_BLOCKS (CFG_UF2_FLASH_SIZE / 256 + 100)
+#define MAX_BLOCKS (CFG_UF2_FLASH_SIZE / 256 + 100 + 262144)
 typedef struct {
-    uint32_t numBlocks;
-    uint32_t numWritten;
+  uint32_t numBlocks;
+  uint32_t numWritten;
 
-    bool aborted;             // aborting update and reset
-    bool update_bootloader;   // if updating bootloader (else app)
-    bool has_uicr;            // if containing uicr data
-    bool boot_id_matches;     // if bootloader id in cf2 config matches our VID/PID
+  bool aborted;           // aborting update and reset
+  bool update_bootloader; // if updating bootloader (else app)
+  bool has_uicr;          // if containing uicr data
+  bool boot_id_matches;   // if bootloader id in cf2 config matches our VID/PID
 
-    uint8_t writtenMask[MAX_BLOCKS / 8 + 1];
+  uint8_t writtenMask[MAX_BLOCKS / 8 + 1];
 } WriteState;
 
 typedef struct {
-    // 32 byte header
-    uint32_t magicStart0;
-    uint32_t magicStart1;
-    uint32_t flags;
-    uint32_t targetAddr;
-    uint32_t payloadSize;
-    uint32_t blockNo;
-    uint32_t numBlocks;
-    uint32_t familyID;
+  // 32 byte header
+  uint32_t magicStart0;
+  uint32_t magicStart1;
+  uint32_t flags;
+  uint32_t targetAddr;
+  uint32_t payloadSize;
+  uint32_t blockNo;
+  uint32_t numBlocks;
+  uint32_t familyID;
 
-    // raw data;
-    uint8_t data[476];
+  // raw data;
+  uint8_t data[476];
 
-    // store magic also at the end to limit damage from partial block reads
-    uint32_t magicEnd;
+  // store magic also at the end to limit damage from partial block reads
+  uint32_t magicEnd;
 } UF2_Block;
 
 void uf2_init(void);
